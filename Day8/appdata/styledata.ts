ea48174ed20ed4c84ec44fc17b4545fe6871f762
Madhan123 <madhankumarreddy012@gmail.com>
2023-04-14 10:31:03 +0530

    Day8

---
+++ Day8/appdata/styledata.ts
@@ -0,0 +1,48 @@
+export const theme1:any =  {
+    h1: {
+    borderBottom: "2px solid black",
+    paddingBottom: "20px",
+    color: "rgb(23, 23, 47)",
+    backgroundColor: "blue",
+    margin: "0px"
+},
+services:{
+    backgroundColor: "pink",
+    height: "380px",
+    boxSizing: "border-box",
+    padding: "20px"
+}
+,footer: {
+    paddingBottom: "10px",
+    color: "rgb(23, 23, 47)",
+    backgroundColor: "red",
+    borderTop: "2px solid black",
+    fontFamily: "Arial, Helvetica, sans-serif",
+    textAlign: "center"
+}
+}
+
+
+export const theme2:any =  {
+    h1: {
+    borderBottom: "2px solid black",
+    paddingBottom: "20px",
+    color: "rgb(23, 23, 47)",
+    backgroundColor: "green",
+    margin: "0px"
+},
+services:{
+    backgroundColor: "pink",
+    height: "380px",
+    boxSizing: "border-box",
+    padding: "20px"
+}
+,footer: {
+    paddingBottom: "10px",
+    color: "rgb(23, 23, 47)",
+    backgroundColor: "#1F618D",
+    borderTop: "2px solid black",
+    fontFamily: "Arial, Helvetica, sans-serif",
+    textAlign: "center"
+}
+}
